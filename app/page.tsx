3709b22ec7c37294f3308affc4e39841c8e6c6b8
rajbhoyar729 <rabhoyarr729@gmail.com>
2025-01-06 16:29:52 +0530

    fixed merge issue

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import ChatBot from './components/ChatBot';
-import BarbaWrapper from './components/BarbaWrapper';
 import ClientWrapper from './components/ClientWrapper';
 
-// Dynamically import Hero with SSR disabled
+// Dynamically import components that use browser APIs
 const Hero = dynamic(() => import('./components/Hero'), { ssr: false });
+const DynamicBarbaWrapper = dynamic(() => import('./components/BarbaWrapper'), { ssr: false });
 
 export default function Home() {
   return (
@@ -20,14 +20,13 @@ export default function Home() {
       <About />
       <Skills />
       <Experience />
-      <BarbaWrapper>
+      <DynamicBarbaWrapper>
         <Projects />
-      </BarbaWrapper>
-      <Contact />
-      {/* Wrap ChatBot with ClientWrapper */}
-      <ClientWrapper>
-        <ChatBot />
-      </ClientWrapper>
+        <Contact />
+        <ClientWrapper>
+          <ChatBot />
+        </ClientWrapper>
+      </DynamicBarbaWrapper>
     </main>
   );
 }
